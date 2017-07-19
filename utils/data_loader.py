746ea806ae2e0aa5725fe2c7a6925669848dad65
zihao.chen_tp <zihao.chen@moji.com>
2017-07-19 09:24:03 +0800

    创建一个数据动态加载工具

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -29,7 +29,7 @@ class data_loader_(object):
         self.nb_classes = nb_classes
         self.onehot = onehot
     def load_keys(self):
-        file_d = open('../all_pic_infs.pkl', 'rb')
+        file_d = open('all_pic_infs.pkl', 'rb')
         pic_data = pickle.load(file_d)
         # print pic_data
         pic_data = pic_data.items()
