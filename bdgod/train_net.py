7a3aa718af62b9a220d7d6570be8a2a9485e38dd
zihao.chen_tp <zihao.chen@moji.com>
2017-07-20 22:36:20 +0800

    添加说明

--- bdgod/train_net.py
+++ bdgod/train_net.py
@@ -156,10 +156,10 @@ def main():
     epochs = 1000
     for e in range(epochs):
         cost = 0.0
-        if e==13:
+        if e==8:
             for param_group in optimizer.param_groups:
                 param_group['lr'] = param_group['lr'] * 0.1
-        elif e==5:
+        elif e==4:
             for param_group in optimizer.param_groups:
                 param_group['lr'] = param_group['lr'] * 0.1
 
