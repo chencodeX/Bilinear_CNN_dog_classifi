010009edd688132cce6cdc1e5a2b88e8b7b44d2b
zihao.chen_tp <zihao.chen@moji.com>
2017-07-22 20:54:36 +0800

    多模型ensemble

--- bdgod/ensemble.py
+++ bdgod/ensemble.py
@@ -5,8 +5,14 @@
 import torch
 from torch.autograd import Variable
 
-
+from torch.utils import model_zoo
+from dog_config import *
 model = torch.load('models/better1.pkl')
 print model
 for p in model.parameters():
     print p.size()
+
+state_dict = model_zoo.load_url('https://download.pytorch.org/models/resnet50-19c8e357.pth', Model_Root)
+for name, param in state_dict.items():
+    print name
+    print param.size()
