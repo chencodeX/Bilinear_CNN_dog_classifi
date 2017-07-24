6a35d3cc58a1a5ceb2ff8f4cecb6ede6c5b5c014
zihao.chen_tp <zihao.chen@moji.com>
2017-07-24 17:31:30 +0800

    多模型ensemble

--- bdgod/train_net.py
+++ bdgod/train_net.py
@@ -162,7 +162,7 @@ def main():
     epochs = 1000
     for e in range(epochs):
         cost = 0.0
-        if e==6:
+        if e==5:
             for param_group in optimizer.param_groups:
                 param_group['lr'] = param_group['lr'] * 0.1
         # elif e==4:
