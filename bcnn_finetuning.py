aae499de9bb57df4ec884390dd316cb2ebabb8bd
zihao.chen_tp <zihao.chen@moji.com>
2017-07-27 12:28:12 +0800

    tf Xception model

--- bcnn_finetuning.py
+++ bcnn_finetuning.py
@@ -326,7 +326,7 @@ if __name__ == '__main__':
     print([_.name for _ in vgg.parameters])
 
 
-    optimizer = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9).minimize(loss)
+    optimizer = tf.train.MomentumOptimizer(learning_rate=0.0003, momentum=0.9).minimize(loss)
     
     check_op = tf.add_check_numerics_ops()
 
