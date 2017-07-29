5c6944e2ad64118685ce63e14b96e530d4a9bf71
zihao.chen_tp <zihao.chen@moji.com>
2017-07-29 20:00:40 +0800

    debug

--- bdgod/train_net.py
+++ bdgod/train_net.py
@@ -4,7 +4,7 @@ import sys
 
 sys.path.append("/mnt/git/Bilinear_CNN_dog_classifi/")
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 print sys.path
 import numpy as np
 import torch
@@ -58,8 +58,8 @@ def adjust_learning_rate(optimizer, epoch):
 
 def main():
     torch.manual_seed(23)
-    Band_num = 1
-    Tag_id = 2
+    Band_num = 2
+    Tag_id = 0
     data_l = data_loader_(batch_size=64, band_num=Band_num, tag_id=Tag_id, shuffle=True, data_add=2, onehot=False,
                           data_size=299, nb_classes=100)
     print data_l.train_length
