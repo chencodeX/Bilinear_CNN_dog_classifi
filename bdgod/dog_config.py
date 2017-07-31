e3c4571319e5408228f4e023c5c874a1bc946e98
zihao.chen_tp <zihao.chen@moji.com>
2017-07-31 18:16:07 +0800

    debug

--- bdgod/dog_config.py
+++ bdgod/dog_config.py
@@ -5,7 +5,7 @@
 Image_Path = '/mnt/git/data/all_img'
 AugMent_Image_Path = '/home/meteo/zihao.chen/filter_ext/img_augmen_data/'
 Model_Root = '/mnt/git/Bilinear_CNN_dog_classifi/models/pretrained_models'
-Test_Image_Path = '/mnt/git/data/test_img'
+Test_Image_Path = '/mnt/git/data/test1'
 
 
 # MEAN_VALUE = [110.61220668946963, 127.15494646868024, 135.13440353131017]
