54df3ccbd63df6c7068a8af7c9add9aa3978b28d
zihao.chen_tp <zihao.chen@moji.com>
2017-08-03 18:37:32 +0800

    densenet train

--- bdgod/train_net.py
+++ bdgod/train_net.py
@@ -180,7 +180,7 @@ def main():
     for e in range(epochs):
         cost = 0.0
         train_acc = 0.0
-        if e == 8:
+        if e == 12:
             for param_group in optimizer.param_groups:
                 param_group['lr'] = param_group['lr'] * 0.3
 
