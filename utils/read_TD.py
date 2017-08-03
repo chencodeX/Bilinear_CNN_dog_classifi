00be402c49b8de0b36fd1ccce2596c2cab49472b
zihao.chen_tp <zihao.chen@moji.com>
2017-08-03 14:06:50 +0800

    目标检测

--- utils/read_TD.py
+++ utils/read_TD.py
@@ -4,8 +4,9 @@
 # from bdgod.dog_config import *
 import cv2
 import pickle
+import numpy as np
 
-Image_Path = '/mnt/git/data/image'
+Image_Path = '/mnt/git/data/image_cp'
 def read_txt():
     all_pic_infs = {}
     inf = open('detres_sense001_test.txt', 'rb')
@@ -31,15 +32,25 @@ def read_txt():
             for locas in all_locas:
                 a_points = locas.split(',')
                 a_points = map(int,a_points)
+                img_temp = cv2.imread(new_path)
+                img_temp_arr = np.array(img_temp)
+                if a_points[0] < 0:
+                    a_points[0] = 0
+                if a_points[1] < 0:
+                    a_points[1] = 0
+                corp_img = img_temp_arr[a_points[1]:a_points[3], a_points[0]:a_points[2]]
+                cv2.imwrite(new_path,corp_img)
                 temp_list.append(a_points)
+                break
+
         if not all_pic_infs.has_key(new_path):
             all_pic_infs[new_path]=temp_list
         all_pic_infs[new_path] = temp_list
         print new_path ,' ',temp_list
-    file_d = open('all_test_pic_infs.pkl','wb')
-
-    pickle.dump(all_pic_infs,file_d)
-    file_d.close()
+    # file_d = open('all_test_pic_infs.pkl','wb')
+    #
+    # pickle.dump(all_pic_infs,file_d)
+    # file_d.close()
 
 
 
