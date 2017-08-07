7abf19ed0a6acaa183104d51737ea1da99cba94b
zihao.chen_tp <zihao.chen@moji.com>
2017-08-07 19:11:36 +0800

    ensemble

--- bdgod/ensemble.py
+++ bdgod/ensemble.py
@@ -7,18 +7,42 @@ sys.path.append("/mnt/git/Bilinear_CNN_dog_classifi/")
 import torch
 import numpy as np
 import cv2
+from torch import optim
+from fc_net import Fc_Net
 from torch.autograd import Variable
 from utils.cv_data_loder import data_loader_
+from progressbar import *
 import random
 from dog_config import *
+
+
 
 def predict(model, x_val):
     x = Variable(x_val.cuda(), requires_grad=False)
     output = model.forward(x)
     if type(output) == tuple:
-        print output[1].size()
-        return output[1].cpu().data.numpy()
+        return output[0].cpu().data.numpy().argmax(axis=1)
     return output.cpu().data.numpy().argmax(axis=1)
+
+
+def train(model, loss, optimizer, x_val, y_val):
+    x = Variable(x_val.cuda(), requires_grad=False)
+    y = Variable(y_val.cuda(), requires_grad=False)
+    optimizer.zero_grad()
+    fx = model.forward(x)
+    # inecption 网络特有
+    if type(fx) == tuple:
+        t_y = fx[0].cpu().data.numpy().argmax(axis=1)
+        acc = 1. * np.mean(t_y == y_val.numpy())
+        output = loss.forward(fx[0], y)
+    else:
+        t_y = fx.cpu().data.numpy().argmax(axis=1)
+        acc = 1. * np.mean(t_y == y_val.numpy())
+        output = loss.forward(fx, y)
+    output.backward()
+    optimizer.step()
+    return output.cuda().data[0], acc
+
 
 def preprocess_input(x):
     x /= 255.
@@ -29,12 +53,12 @@ def preprocess_input(x):
 
 def main():
     data_l = data_loader_(batch_size=64, band_num=1, tag_id=0, shuffle=False, data_add=4, onehot=False,
-                 data_size=224,nb_classes=100)
+                          data_size=224, nb_classes=100)
 
     model = torch.load('models/resnet101_model_pretrained_SGD_16_498_1.pkl')
     model.training = False
     num_batches = data_l.test_length / data_l.batch_szie
-    all_data = np.zeros((0,2048)).astype(np.float)
+    all_data = np.zeros((0, 2048)).astype(np.float)
     all_lable = np.zeros((0))
     for j in range(num_batches + 1):
         teX, teY = data_l.get_test_data()
@@ -49,7 +73,7 @@ def main():
         # futures = predict(model, teX)
         # print futures.shape
         # all_data = np.concatenate((all_data,futures),axis=0)
-        all_lable = np.concatenate((all_lable,teY),axis=0)
+        all_lable = np.concatenate((all_lable, teY), axis=0)
 
     # print all_data.shape
     print all_lable.shape
@@ -61,5 +85,57 @@ def main():
     # np.save('future_densenet161.npy',all_data)
     np.save('lable_resnet101.npy', all_lable)
 
+
+def train():
+    inception_data = np.load('feature_inception_v3.npy').astype(np.float)
+    densenet_data = np.load('feature_densenet161.npy').astype(np.float)
+    resnet_data = np.load('feature_resnet101.npy').astype(np.float)
+    lable = np.load('lable_resnet101.npy')
+    all_data = np.concatenate((inception_data, densenet_data, resnet_data), axis=1)
+    proportion = 0.8
+    batch_size = 64
+    train_X = all_data[:int(all_data.shape[0]*proportion)]
+    test_X = all_data[int(all_data.shape[0] * proportion):]
+
+    train_Y = lable[:int(lable.shape[0]*proportion)]
+    test_Y = lable[int(lable.shape[0]*proportion):]
+    model = Fc_Net(all_data.shape[1],lable.shape[1])
+    model = model.cuda()
+    loss = torch.nn.CrossEntropyLoss(size_average=True)
+    loss = loss.cuda()
+
+    optimizer = optim.SGD(model.parameters(), lr=(0.001), momentum=0.9, weight_decay=0.0005)
+
+    epochs = 1000
+    for e in range(epochs):
+        num_batches_train = int(train_X.shape[0] / batch_size)+1
+        train_acc= 0.0
+        cost = 0.0
+        widgets = ['Progress: ', Percentage(), ' ', Bar(marker=RotatingMarker('>'))]
+        pbar = ProgressBar(widgets=widgets, maxval=num_batches_train)
+        pbar.start()
+        for i in range(num_batches_train):
+            start, end = i * batch_size, (i + 1) * batch_size
+            batch_trX = train_X[start:end]
+            batch_trY = train_Y[start:end]
+            tor_batch_trX = torch.from_numpy(batch_trX)
+            tor_batch_trY = torch.from_numpy(batch_trY)
+            cost_temp, acc_temp = train(model, loss, optimizer, tor_batch_trX, tor_batch_trY)
+            train_acc += acc_temp
+            cost += cost_temp
+            pbar.update(i)
+        pbar.finish()
+        print 'Epoch %d ,all average train loss is : %f' % (e,cost / (num_batches_train))
+        print 'Epoch %d ,all average train acc is : %f' % (e,train_acc / (num_batches_train))
+        acc = 0.0
+        num_batches_test = int(test_X.shape[0] / batch_size)+1
+        for j in range(num_batches_test):
+            start, end = j * batch_size, (j + 1) * batch_size
+            predY = predict(model, torch.from_numpy(test_X[start:end]))
+            acc += 1. * np.mean(predY == test_Y[start:end])
+
+        print 'Epoch %d ,all test acc is : %f' % (e,acc / num_batches_test)
+
+
 if __name__ == '__main__':
-    main()
+    train()
--- bdgod/fc_net.py
+++ bdgod/fc_net.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 class Fc_Net(nn.Module):
     def __init__(self,input_fetures,num_classes=100):
         super(Fc_Net, self).__init__()
+        self.training = True
         self.fc1 = nn.Linear(input_fetures, 2048)
         self.rl1 = nn.ReLU(inplace=True)
         self.dropout1 = nn.Dropout()
@@ -18,6 +19,12 @@ class Fc_Net(nn.Module):
 
     def forward(self,x):
         x = self.fc1(x)
+        x = self.rl1(x)
+        if self.training:
+            x = self.dropout1(x)
         x = self.fc2(x)
+        x = self.rl2(x)
+        if self.training:
+            x = self.dropout2(x)
         x = self.fc3(x)
         return x
