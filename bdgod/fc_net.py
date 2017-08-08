3f8c765dae58e4f9a14ff0102217614568884ac8
zihao.chen_tp <zihao.chen@moji.com>
2017-08-08 10:20:56 +0800

    ensemble

--- bdgod/fc_net.py
+++ bdgod/fc_net.py
@@ -9,10 +9,10 @@ class Fc_Net(nn.Module):
     def __init__(self,input_fetures,num_classes=100):
         super(Fc_Net, self).__init__()
         self.training = True
-        self.fc1 = nn.Linear(input_fetures, 512)
+        self.fc1 = nn.Linear(input_fetures, 1024)
         self.rl1 = nn.ReLU(inplace=True)
         self.dropout1 = nn.Dropout()
-        self.fc2 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(1024, 256)
         self.rl2 = nn.ReLU(inplace=True)
         self.dropout2 = nn.Dropout()
         self.fc3 = nn.Linear(256, num_classes)
